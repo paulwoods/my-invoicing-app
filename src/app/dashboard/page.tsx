f38ad148f6b17d156f8ba95ee5fcf27a118b07b5
mrpaulwoods <mr.paul.woods@gmail.com>
2025-03-02 19:35:20 -0600

    feat: implemented invoice view

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,111 @@
+import {Badge} from "@/components/ui/badge"
+import {Button} from "@/components/ui/button";
+
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+
+import {CirclePlus} from 'lucide-react';
+import Link from "next/link";
+
+export default function Home() {
+    return (
+        <main className="h-96 flex flex-col justify-center text-center gap-6 max-w-5xl mx-auto my-12">
+
+            <div className="flex justify-between">
+                <h1 className="text-3xl font-semibold ">
+                    Invoices
+                </h1>
+                <p>
+                    <Button className="inline-flex gap-2" variant="ghost" asChild>
+                        <Link href="/invoices/new">
+                            <CirclePlus className="h-4 w-4"/>
+                            Create Invoice
+                        </Link>
+                    </Button>
+                </p>
+            </div>
+
+            <Table>
+                <TableCaption>A list of your recent invoices.</TableCaption>
+                <TableHeader>
+                    <TableRow>
+
+                        <TableHead className="w-[100px] p-4">
+                            <span>
+                                Date
+                            </span>
+                        </TableHead>
+
+                        <TableHead className="p-4">
+                            <span>
+                                Customer
+                            </span>
+                        </TableHead>
+
+                        <TableHead className="p-4">
+                            <span>
+                                Email
+                            </span>
+                        </TableHead>
+
+                        <TableHead className="text-center p-4">
+                            <span>
+                                Status
+                            </span>
+                        </TableHead>
+
+                        <TableHead className="text-right p-4">
+                            <span>
+                                Value
+                            </span>
+                        </TableHead>
+
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    <TableRow>
+
+                        <TableCell className="text-left font-bold p-4">
+                            <span className="font-semibold">
+                                10/31/2024
+                            </span>
+                        </TableCell>
+
+                        <TableCell className="text-left font-bold p-4">
+                            <span className="font-semibold">
+                                Philip J. Fry
+                            </span>
+                        </TableCell>
+
+                        <TableCell className="text-left p-4">
+                            <span className="">
+                                fry@example.com
+                            </span>
+                        </TableCell>
+
+                        <TableCell className="text-center p-4">
+                            <Badge className="rounded-full">
+                                Open
+                            </Badge>
+                        </TableCell>
+
+                        <TableCell className="text-right p-4">
+                            <span className="font-semibold">
+                                $250.00
+                            </span>
+                        </TableCell>
+
+                    </TableRow>
+                </TableBody>
+            </Table>
+
+        </main>
+    );
+}
---
+++ src/app/invoices/new/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+    return (
+        <main className="h-96 flex flex-col justify-center text-center gap-6 max-w-5xl mx-auto my-12">
+
+            <div className="flex justify-between">
+                <h1 className="text-3xl font-semibold ">
+                    Create a New Invoice
+                </h1>
+            </div>
+
+
+
+        </main>
+    );
+}
