4aa73d27bab676591d5ae3776beb0f6a2eadf19b
mrpaulwoods <mr.paul.woods@gmail.com>
2025-03-02 20:00:49 -0600

    feat(ui): add reusable input, label, and textarea components
    Introduced `Input`, `Label`, and `Textarea` components to enhance UI consistency and reusability. Integrated these components into the invoice creation form for better maintainability and styling flexibility.

--- src/app/invoices/new/page.tsx
+++ src/app/invoices/new/page.tsx
@@ -1,6 +1,11 @@
+import {Input} from "@/components/ui/input"
+import {Label} from "@/components/ui/label"
+import {Textarea} from "@/components/ui/textarea"
+import {Button} from "@/components/ui/button"
+
 export default function Home() {
     return (
-        <main className="h-96 flex flex-col justify-center text-center gap-6 max-w-5xl mx-auto my-12">
+        <main className="h-96 flex flex-col justify-center gap-6 max-w-5xl mx-auto my-12">
 
             <div className="flex justify-between">
                 <h1 className="text-3xl font-semibold ">
@@ -8,7 +13,29 @@ export default function Home() {
                 </h1>
             </div>
 
-
+            <form className="grid gap-4 max-w-xs">
+                <div>
+                    <Label htmlFor="name" className="block mb-2 font-semibold text-sm">Billing Name</Label>
+                    <Input id="name" name="name" type="text"/>
+                </div>
+                <div>
+                    <Label htmlFor="email" className="block mb-2 font-semibold text-sm">Billing Email</Label>
+                    <Input id="email" name="email" type="email"/>
+                </div>
+                <div>
+                    <Label htmlFor="value" className="block mb-2 font-semibold text-sm">Value</Label>
+                    <Input id="value" name="value" type="email"/>
+                </div>
+                <div>
+                    <Label htmlFor="description" className="block mb-2 font-semibold text-sm">Description</Label>
+                    <Textarea id="description" name="description"></Textarea>
+                </div>
+                <div>
+                    <Button className="w-full font-semibold">
+                        Submit
+                    </Button>
+                </div>
+            </form>
 
         </main>
     );
